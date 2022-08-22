0b52baae635f6ce57a37346e0247268b33dfa3b5
Edwin <edgoca2498@gmail.com>
2022-08-21 20:48:12 -0600

    chore(feature/PSL-11): Dockerización de proyecto

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        watch: {
+            usePolling: true
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
