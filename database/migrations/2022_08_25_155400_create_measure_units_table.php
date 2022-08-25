def90a8618a3f7b8a31b300f4a3eabf7560a12f9
Álvaro Flores - E&A <moralesalvaro308@gmail.com>
2022-08-25 10:22:31 -0600

    Agregando v1 tablas. Flujo Módulo V1

---
+++ database/migrations/2022_08_25_155400_create_measure_units_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeasureUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('measure_units', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('measure_units')->comment('Nombre de la unidad de medida');
+            $table->string('descripción')->comment('Descripción, comentarios sobre la unidad de medida');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('measure_units');
+    }
+}
