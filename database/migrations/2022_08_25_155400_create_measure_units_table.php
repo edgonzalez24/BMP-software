d7c32e7075e2460f5674833a91eb3870a44ed2c5
Álvaro Flores - E&A <moralesalvaro308@gmail.com>
2022-09-02 14:59:26 -0600

    shore (feature/PDSL-37): Mejora DB Módulo 1 v2

--- database/migrations/2022_08_25_155400_create_measure_units_table.php
+++ database/migrations/2022_08_25_155400_create_measure_units_table.php
@@ -17,6 +17,7 @@ class CreateMeasureUnitsTable extends Migration
             $table->increments('id');
             $table->string('measure_units')->comment('Nombre de la unidad de medida');
             $table->string('descripción')->comment('Descripción, comentarios sobre la unidad de medida');
+            $table->boolean('active')->default(true)->comment('Marca a la unidad de medida como activo o inactivo ');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_09_02_201016_create_presales_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('total_paid')->comment('Total de monto pagado de esta venta');
+            $table->float('total_pending')->comment('Total de monto pendiente de pago de esta venta');
+            $table->boolean('dispatch')->comment('Marca como despachada o pendiente de despacha')->default(false);
+            $table->unsignedInteger('client_id')->comment('ID del cliente');
+            $table->unsignedInteger('user_presale_id')->comment('ID usuario de preventa ');
+            $table->unsignedInteger('user_dispatch_id')->comment('ID de usuario que despacha la preventa');
+
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('user_presale_id')->references('id')->on('users');
+            $table->foreign('user_dispatch_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presales');
+    }
+};
