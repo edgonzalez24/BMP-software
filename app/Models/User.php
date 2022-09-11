38ba948ade61a4e6976022d807aee461a22862d6
ÁlvaroFloresHome <moralesalvaro308@gmail.com>
2022-09-10 19:06:34 -0600

    chore(feature/PDSL-58): Refactor: Eliminar Usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,7 @@ class User extends Authenticatable
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
