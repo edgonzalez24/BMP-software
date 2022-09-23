c8d2808ce4bfe25f1f945c1c074d0396c17a303c
Edwin <edgoca2498@gmail.com>
2022-09-22 18:47:29 -0600

    chore(feature/PDSL-81): trabajo en progreso

--- resources/js/Pages/Suppliers/Show.vue
+++ resources/js/Pages/Suppliers/Show.vue
@@ -7,9 +7,10 @@ import JetLabel from '@/Components/Label.vue';
 import JetInput from '@/Components/Input.vue';
 import Toggle from '@/Components/Shared/Toggle.vue';
 import { useForm } from '@inertiajs/inertia-vue3';
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import Table from '@/Components/Table.vue';
 import Status from '@/Components/Shared/Status.vue';
+import Pagination from '@/Components/Shared/Pagination.vue';
 
 const props = defineProps({
   suppliers: Object
@@ -39,16 +40,27 @@ const header = reactive([
 const isLoading = ref(false);
 const isEdit = ref(false);
 const statusModalForm = ref(false);
+const statusModalDelete = ref(false);
 const formInitial = useForm({
   name: null,
   telephone: null,
   email: null,
   active: true
 });
-
+const formDelete = useForm({
+  id: null
+});
 const toggleFormModal = () => {
   statusModalForm.value = !statusModalForm.value;
 };
+const toggleDeleteModal = () => {
+  statusModalDelete.value = !statusModalDelete.value;
+};
+const selectDeleteItem = item => {
+  formDelete.id = item.id;
+  toggleDeleteModal();
+};
+const totalPages = computed(() => Math.ceil(props.suppliers.total / props.suppliers.per_page));
 </script>
 
 <template>
@@ -113,6 +125,36 @@ const toggleFormModal = () => {
         </div>
       </form>
     </JetModal>
+    <JetModal :show="statusModalDelete" maxWidth="lg" @close="toggleDeleteModal" >
+      <form class="py-8 px-5" @submit.prevent="submitDelete">
+        <h2 class="font-semibold text-2xl text-dark-blue-500 leading-tight text-center mb-5">
+          Eliminar Proveedor
+        </h2>
+        <div class="px-5">
+          <p class="mt-5 text-justify text-gray-400">
+            Al  eliminar a este proveedor se borrará permanentemente del sistema,
+            por favor confirmar la acción haciendo click en el botón de 'Eliminar'.
+          </p>
+          <div class="flex justify-end mt-5">
+            <div class="w-auto flex flex-row space-x-4 justify-between">
+              <JetButton
+                background="bg-transparente text-gray-300 focus:ring-transparent focus:border-transparent"
+                type="button"
+                @click="toggleDeleteModal"
+              >
+                Cancelar
+              </JetButton>
+              <JetButton
+                background="bg-red-600 focus:ring-transparent focus:border-transparent"
+                type="submit"
+              >
+                Eliminar
+              </JetButton>
+            </div>
+          </div>
+        </div>
+      </form>
+    </JetModal>
     <div class="min-h-screen">
       <div class="max-w-7xl mx-auto px-5 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center my-5">
@@ -145,13 +187,21 @@ const toggleFormModal = () => {
                   <div class="flex justify-center">
                     <div class="flex flex-row space-x-4">
                       <a @click="true" class="text-blue-500 font-medium cursor-pointer">Editar</a>
-                      <a @click="true" class="text-blue-500 font-medium cursor-pointer">Eliminar</a>
+                      <a @click="selectDeleteItem(item)" class="text-blue-500 font-medium cursor-pointer">Eliminar</a>
                     </div>
                   </div>
                 </td>
               </tr>
             </tbody>
           </Table>
+        </div>
+        <div class="flex mt-8 justify-center">
+          <Pagination
+            :total="totalPages"
+            :previous="suppliers.prev_page_url"
+            :next="suppliers.next_page_url"
+            page="suppliers"
+          />
         </div>
       </div>
     </div>
