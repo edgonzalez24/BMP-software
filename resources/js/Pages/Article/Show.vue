ab17f6980e5ef48ccc80537f955ad8664ff10790
Edwin <edgoca2498@gmail.com>
2022-09-27 15:42:22 -0600

    chore(feature/PDSL-90): Trabajo en progreso

---
+++ resources/js/Pages/Article/Show.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+  <AppLayout>
+    <div class="min-h-screen">
+      <div class="max-w-7xl mx-auto px-5 sm:px-6 lg:px-8">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias debitis sunt inventore corrupti nisi tenetur perferendis expedita ducimus quasi, dolor soluta explicabo maiores, ea iusto saepe repellendus est doloribus voluptatibus!
+      </div>
+    </div>
+  </AppLayout>
+</template>
