e5ed019613d5899e3ccfeca8ad99dcae9a59f2b8
Edwin <edgoca2498@gmail.com>
2022-10-17 22:15:28 -0600

    chore(feature/PDSL-100): force to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider;\
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- resources/js/Pages/Order/Show.vue
+++ resources/js/Pages/Order/Show.vue
@@ -1,6 +1,30 @@
 <script setup>
   import AppLayout from '@/Layouts/AppLayout.vue';
   import JetButton from '@/Components/Button.vue';
+  import { reactive, computed, ref, getCurrentInstance } from 'vue';
+
+  const header = reactive([
+    {
+      name: 'ID',
+      showInMobile: true
+    },
+    {
+      name: 'Tipo de cliente',
+      showInMobile: false
+    },
+    {
+      name: 'Estado',
+      showInMobile: true
+    },
+    {
+      name: 'Télefono',
+      showInMobile: true
+    },
+    {
+      name: 'Acciones',
+      showInMobile: true
+    }
+  ]);
 </script>
 <template>
   <AppLayout>
@@ -13,6 +37,35 @@
           <JetButton @click="true">
             Crear
           </JetButton>
+        </div>
+        <div class="bg-white w-full sm:overflow-x-hidden overflow-x-auto shadow-xl rounded-lg min-h-base">
+          <Table :header="header">
+            <tbody class="px-5">
+              <!-- <tr v-for="item in clients.data" class="mt-2 cursor-pointer hover:bg-slate-50 transition duration-300 ease-in-out"
+                @click="selectDetail(item)">
+                <td class="text-center p-2 md:text-base text-xs">{{ item.name }}</td>
+                <td class="text-center p-2 md:text-base text-xs">{{ getTypeClient(item.type_client_id) }}</td>
+                <td class="text-center p-2 md:text-base text-xs">
+                  <div class="flex justify-center">
+                    <Status :status="item.active" class="sm:w-1/2 md:w-1/3 w-full" />
+                  </div>
+                </td>
+                <td class="text-center p-2 md:text-base text-xs" @click.stop>
+                  <a :href="`tel:${item.telephone}`">
+                    {{ item.telephone || '-' }}
+                  </a>
+                </td>
+                <td class="text-center p-2 md:text-base text-xs" @click.stop>
+                  <div class="flex justify-center">
+                    <div class="flex flex-row space-x-4">
+                      <a @click="selectItem(item)" class="text-blue-500 font-medium cursor-pointer">Editar</a>
+                      <a @click="selectDeleteItem(item)" class="text-blue-500 font-medium cursor-pointer">Eliminar</a>
+                    </div>
+                  </div>
+                </td>
+              </tr> -->
+            </tbody>
+          </Table>
         </div>
       </div>
     </div>
