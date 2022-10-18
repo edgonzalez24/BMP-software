fbdbe30495fbccf5f74bb14e0a9851f3cf11a346
Edwin <edgoca2498@gmail.com>
2022-10-17 23:34:12 -0600

    chore(feature/PDSL-100): force to https ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('development')) {
+        if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
     }
