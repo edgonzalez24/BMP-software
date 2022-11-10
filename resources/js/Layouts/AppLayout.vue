6a72129ac862e76a150eabe741da605a58947396
Edwin <edgoca2498@gmail.com>
2022-11-10 12:32:23 -0600

    chore(feature/PDSL-158) Agregar politicas de privacidad

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -52,27 +52,16 @@ const userName = computed(() => usePage().props.value.user.name)
 				<div class="md:w-9/12	lg:w-5/6 w-full">
 					<!-- Page Heading -->
 					<header class="bg-white shadow">
-						<div class="max-w-7xl mx-auto py-6 md:py-4 px-4 sm:px-6 lg:px-8 flex justify-end">
-							<!-- <div>
-								<JetDropdown
-									align="left"
-								>
-									<template #trigger>
-										<div class="relative inline-flex transition duration-300 ease-in-out hover:opacity-80 cursor-pointer">
-											<font-awesome-icon icon="fa-regular fa-bell" class="text-2xl text-gray-400" @click="true" />
-											<span class="flex absolute h-3 w-3 top-0 right-0 -mt-2 -mr-1">
-												<span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-												<span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
-											</span>
-										</div>
-									</template>
-									<template #content>
-										<div class="p-2">
-											Lorem ipsum dolor
-										</div>
-									</template>
-								</JetDropdown>
-							</div> -->
+						<div class="max-w-7xl mx-auto py-6 md:py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+							<div class="flex items-center h-full">
+								<a href="https://github.com/edgonzalez24/BMP-software" target="_blank" class="text-gray-400 hover:text-dark-blue-500 inline-flex items-center text-sm transition duration-500 ease-in-out">
+									<svg class="fill-current h-8 w-8 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+										<path
+											d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+									</svg>
+									Github
+								</a>
+							</div>
 							<div class="flex items-center">
 								<div class="relative">			
 									<JetDropdown>
@@ -95,7 +84,7 @@ const userName = computed(() => usePage().props.value.user.name)
 									</JetDropdown>
 								</div>
 								<div class="md:hidden block ml-2" @click="statusMenu = !statusMenu">
-									<font-awesome-icon icon="fa-solid fa-bars" class="text-xl text-gray-400 cursor-pointer" />
+									<font-awesome-icon icon="fa-solid fa-bars" class="text-2xl text-gray-400 cursor-pointer" />
 								</div>
 							</div>
 						</div>
