08964e9ad9e17b2f6f865f01d85f1795975a5c6d
Edwin <edgoca2498@gmail.com>
2022-11-12 07:43:12 -0600

    chore(feature/PDSL-11): Agregar reportes a graficas

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,25 +8,18 @@
   import moment from 'moment';
   import 'moment/dist/locale/es';
   import Table from '@/Components/Table.vue';
+  import _ from 'lodash';
 
-  const user = computed(() => usePage().props.value.user.name);
-  const dummy = [
-    {
-      kind: 'orders',
-      description: 'Ordenes',
-      value: 20
-    },
-    {
-      kind: 'completed',
-      description: 'Ordenes completadas',
-      value: 30
-    },
-    {
-      kind: 'presales',
-      description: 'Total de Ventas',
-      value: 50
-    }
-  ]
+
+
+  const props = defineProps({
+    order_total: Number,
+    orders_complete: Number,
+    total_sale: Number,
+    top_articles: Array,
+    top_clients: Array
+  });
+
   const date = moment().locale("es").format('dddd, D [de] MMMM [del] YYYY');
   const header = reactive([
     {
@@ -38,14 +31,55 @@
       showInMobile: true
     },
     {
+      name: 'Categoria',
+      showInMobile: true
+    },
+    {
       name: 'Proveedor',
       showInMobile: true
     },
     {
-      name: 'Precio por Unidad',
+      name: 'Precio',
       showInMobile: true
     },
   ]);
+  const cardsInfo = reactive([
+    {
+      kind: 'orders',
+      description: 'Ordenes',
+      value: props.order_total
+    },
+    {
+      kind: 'completed',
+      description: 'Ventas',
+      value: props.orders_complete
+    },
+    {
+      kind: 'presales',
+      description: 'Total de Ventas',
+      value: `$ ${Number(props.total_sale).toFixed(2) }`
+    }
+  ])
+
+
+  const user = computed(() => usePage().props.value.user.name);
+  const top = computed(() => {
+    const values =  _(props.top_articles)
+      .groupBy('id')
+      .map((item,  index) => ({
+        id: item[index].id,
+        article: item[index].article,
+        sale_price: item[index].sale_price,
+        category: item[index].category,
+        suppliers: item[index].suppliers,
+        total: _.sumBy(item, 'total')
+      }))
+      .value()
+    const order = _.orderBy(values, ['total'], ['desc', 'asc']);
+
+    return _.take(order, 5);
+  })
+  
 </script>
 
 <template>
@@ -58,15 +92,15 @@
         <p class="mb-5">{{ date }}</p>
         <div class="w-full mb-8">
           <div class="grid md:grid-cols-3 xl:gap-x-24 lg:gap-x-20 gap-x-5 md:gap-y-0 gap-y-5">
-            <div v-for="item in dummy">
+            <div v-for="item in cardsInfo">
               <CardPreview  :value="item" />
             </div>
           </div>
         </div>
         <div class="w-full flex justify-between lg:flex-row flex-col lg:space-x-6 lg:space-y-0 space-y-4 mb-8">
           <div class="w-full lg:w-1/2 p-5 bg-white rounded-lg overflow-hidden shadow-card border border-gray-50 animated fadeIn">
-            <h4 class="font-semibold md:text-xl text-base text-dark-blue-500 leading-tight animated zoomIn">Resumen de clientes con más preventas en el mes</h4>
-            <LineChart />
+            <h4 class="font-semibold md:text-xl text-base text-dark-blue-500 leading-tight animated zoomIn">Reporte de clientes con más pedidos en el mes</h4>
+            <LineChart :value="top_clients" />
           </div>
           <div class="w-full lg:w-1/2 p-5 bg-white rounded-lg overflow-hidden shadow-card border border-gray-50 animated fadeIn">
             <h4 class="font-semibold md:text-xl text-base text-dark-blue-500 leading-tight animated zoomIn">Reporte de ventas anual</h4>
@@ -75,10 +109,18 @@
           </div>
         </div>
         <div class="w-full bg-white rounded-lg overflow-hidden shadow-card min-h-table border border-gray-50 p-5 animated fadeIn">
-          <h4 class="font-semibold md:text-xl text-base text-dark-blue-500 leading-tight animated zoomIn">Productos más vendidos</h4>
-          <Table :header="header">
+          <h4 class="font-semibold md:text-xl text-base text-dark-blue-500 leading-tight animated zoomIn">Productos más vendidos en el mes</h4>
+          <Table :header="header" :items="top.length">
             <tbody class="px-5">
-              <tr class="mt-2">
+              <tr 
+                class="mt-2"
+                v-for="item in top"
+              >
+                <td class="text-center p-2 lg:text-base text-xs">#{{ item.id }}</td>
+                <td class="text-center p-2 lg:text-base text-xs">{{ item.article }}</td>
+                <td class="text-center p-2 lg:text-base text-xs">{{ item.category }}</td>
+                <td class="text-center p-2 lg:text-base text-xs">{{ item.suppliers }}</td>
+                <td class="text-center p-2 lg:text-base text-xs">${{ Number(item.sale_price).toFixed(2) }}</td>
               </tr>
             </tbody>
           </Table>
