cc020c8b88f698be2d01978aae40665e9a33450e
Edwin <edgoca2498@gmail.com>
2022-11-17 15:58:15 -0600

    fix(feature/PDSL-11) Agregar meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
